c7dc37ec7b768932741e52a062eae9fa47b3354b
Victor Ramos <vzurielr@gmail.com>
2023-11-09 17:24:34 -0500

    Tag whatsapp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,6 +51,19 @@ export default function RootLayout({
             function gtag(){ window.dataLayer.push(arguments)}
             gtag('js', new Date());
             gtag('config', 'AW-11403069413');
+
+            function gtag_report_conversion(url) {
+              var callback = function () {
+                if (typeof(url) != 'undefined') {
+                  window.location = url;
+                }
+              };
+              gtag('event', 'conversion', {
+                  'send_to': 'AW-11403069413/8fIZCPHSp_UYEOWPtL0q',
+                  'event_callback': callback
+              });
+              return false;
+            }
           `
         }
 
